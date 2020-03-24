e795fd42f6b893a16ba0bbeab29a5f0efc61c6a9
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2020-03-23 19:22:31 -0700

    Update backends.tf

--- linode/backends.tf
+++ linode/backends.tf
@@ -5,3 +5,13 @@ terraform {
     region = "us-east-1"
   }
 }
+
+data "terraform_remote_state" "s3" {
+  backend = "s3"
+
+  config {
+    bucket = "churchoffoxx"
+    key    = "terraform/linode/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
