10ffdad84e944801b5829f23533dec5d5bd6ab3d
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2020-03-24 18:47:13 -0700

    Updated variables

--- linode/variables.tf
+++ linode/variables.tf
@@ -1,3 +1,8 @@
+variable "authorized_keys" {
+  default = [ "" ]
+  description = "Authorized keys for persistent access"
+}
+
 variable "region" {
   default = "us-central"
   description = "Location to clone instances"
