643183722571da9d40fd53b10e431a2f53462189
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2020-03-24 18:51:56 -0700

    Updated targetting

--- linode/main.tf
+++ linode/main.tf
@@ -3,6 +3,7 @@ module "neko" {
 
   authorized_keys = var.authorized_keys
   image           = "g6-standard-1"
+  name            = "neko"
   region          = var.region
   root_pass       = var.root_pass
 }
