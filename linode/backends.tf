030b172ce37d7385fe9b8729c035e396408f9be3
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2020-03-24 17:59:37 -0700

    Updated backends

--- linode/backends.tf
+++ linode/backends.tf
@@ -1,17 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "churchoffoxx"
+    bucket = var.backend_bucket
     key    = "terraform/linode/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
-data "terraform_remote_state" "s3" {
-  backend = "s3"
-
-  config {
-    bucket = "churchoffoxx"
-    key    = "terraform/linode/terraform.tfstate"
-    region = "us-east-1"
+    region = var.backend_region
   }
 }
---
+++ linode/variables.tf
@@ -0,0 +1,19 @@
+variable "backend_bucket" {
+  default = "churchoffoxx"
+  description = "Bucket to use for the statefile"  
+}
+
+variable "backend_region" {
+  default = "us-east-1"
+  description = "Location of the statefile"
+}
+
+variable "region" {
+  default = "us-central"
+  description = "Location to clone instances"
+}
+
+variable "token" {
+  default = ""
+  description = "Linode API token"
+}
