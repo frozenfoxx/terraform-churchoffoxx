acdbb73e1057a063eb541f59b9d016ceff360f00
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2020-03-26 14:13:44 -0700

    Updated data_sources targeting

--- linode/data_sources.tf
+++ linode/data_sources.tf
@@ -2,6 +2,6 @@ data "terraform_remote_state" "terraform_state" {
   backend = "local"
 
   config = {
-    path = "${path.module}../../terraform.tfstate"
+    path = "${path.root}/.terraform/terraform.tfstate"
   }
 }
