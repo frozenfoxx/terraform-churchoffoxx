fc8caecd3165d26fa3d431e311c8ccdc4ddcafc2
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2020-03-26 20:07:39 -0700

    Updated data sources

--- linode/data_sources.tf
+++ linode/data_sources.tf
@@ -1,3 +1,18 @@
+data "linode_profile" "me" {
+}
+
+data "linode_region" "main" {
+  id = var.region
+}
+
+data "linode_instance_type" "default" {
+  id = "g6-nanode-1"
+}
+
+data "linode_image" "ubuntu" {
+  id = "linode/ubuntu18.04"
+}
+
 data "terraform_remote_state" "terraform_state" {
   backend = "local"
 
