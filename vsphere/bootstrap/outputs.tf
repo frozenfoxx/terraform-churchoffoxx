17f0b08936bb431111bc6a315dd50370014f5a25
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2022-09-25 06:26:43 +0000

    Removed vsphere

--- vsphere/bootstrap/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "bootstrap_Name" {
-  value = module.bootstrap.vm_name
-}
-
-output "bootstrap_Management_IP" {
-  value = module.bootstrap.vm_management_ip
-}
--- vsphere/bootstrap/providers.tf
+++
@@ -1,9 +0,0 @@
-provider "vsphere" {
-  version        = "~> 1.11"
-  user           = var.viuser
-  password       = var.vipassword
-  vsphere_server = var.viserver
-
-  # Allow if you have a self-signed certs in vCenter
-  allow_unverified_ssl = true
-}
